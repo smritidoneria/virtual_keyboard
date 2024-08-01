28c8332a2b00f7e2381884d9870936728de3afdf
Smriti Doneria <smritidoneria@gmail.com>
2024-08-01 19:00:22 +0530

    done with the class

--- main.py
+++ main.py
@@ -5,14 +5,31 @@ from cvzone.HandTrackingModule import HandDetector
 cap = cv2.VideoCapture(0)
 hand_detector = HandDetector(detectionCon=0.8)
 
+
+class Button():
+    def __init__(self, pos, text, size=[95,95]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+    def draw(self, img):
+        x,y=self.pos
+        w,h=self.size
+        cv2.rectangle(img, self.pos, (x+w,y+h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, self.text, (self.pos[0] + 15, self.pos[1] + 75), cv2.FONT_HERSHEY_DUPLEX, 3, (255, 255, 255),
+                    2)
+        return img
+
+
+mybutton = Button([100, 100], "Q")
+
 while True:
     success, img = cap.read()
     # Detect hands in the image
-    hand , img = hand_detector.findHands(img)
+    hand, img = hand_detector.findHands(img)
 
-    cv2.rectangle(img,(100,100),(200,200),(255,0,255),cv2.FILLED)
-    cv2.putText(img,'Q',(115,180),cv2.FONT_HERSHEY_DUPLEX,5,(255,255,255),5)
-     # Display the image
+    img = mybutton.draw(img)
+    # Display the image
     cv2.imshow("Hand Detection", img)
 
     # Break the loop if 'q' is pressed
